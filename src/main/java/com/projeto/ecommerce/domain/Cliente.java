99239b9f53c21ae87d3df63953eb512fc19be61b
Mayara Delonzek <mayaradelonzek2@gmail.com>
2022-01-26 22:02:48 -0300

    add Cliente domain

---
+++ src/main/java/com/projeto/ecommerce/domain/Cliente.java
@@ -0,0 +1,34 @@
+package com.projeto.ecommerce.domain;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Cliente extends Pessoa {
+
+    @NotNull(message = "Nome é obrigatório")
+    @NotEmpty(message = "Nome é obrigatório")
+    @Size(max = 100, message = "Nome precisa ter no máximo 100 caracteres")
+    private String nome;
+
+    @NotNull(message = "CPF é obrigatório")
+    @NotEmpty(message = "CPF é obrigatório")
+    @Size(max = 20, message = "CPF precisa ter no máximo 20 caracteres")
+    private String cpf;
+
+    public Cliente(int id, Contato contato, Endereco endereco, String nome, String cpf) {
+        super(id, contato, endereco);
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
