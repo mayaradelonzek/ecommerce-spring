89242cf63e1b26d8035cc730e414757b9e0a32c9
Mayara Delonzek <mayaradelonzek2@gmail.com>
2022-01-26 21:37:01 -0300

    add Contato domain

---
+++ src/main/java/com/projeto/ecommerce/domain/Contato.java
@@ -0,0 +1,37 @@
+package com.projeto.ecommerce.domain;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Embeddable
+public class Contato {
+
+    @NotNull(message = "E-mail é obrigatório")
+    @NotEmpty(message = "E-mail é obrigatório")
+    @Size(max = 100, message = "E-mail precisa ter no máximo 100 caracteres")
+    private String email;
+
+    @NotNull(message = "Telefone é obrigatório")
+    @NotEmpty(message = "Telefone é obrigatório")
+    @Size(max = 20, message = "Telefone precisa ter no máximo 20 caracteres")
+    private String telefone;
+
+    @Deprecated
+    protected Contato() {}
+
+    public Contato(String email, String telefone) {
+        this.email = email;
+        this.telefone = telefone;
+        //TODO isValid()
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+}
